f4d47b99f47c85c773153133131a5d22a7977f18
Clemens Wijnekus <clemens.wijnekus@ingka.com>
2020-11-03 16:49:33 +0100

    Initial commit

---
+++ src/main/java/com/example/sonarqube/SonarqubeApplication.java
@@ -0,0 +1,12 @@
+package com.example.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SonarqubeApplication {
+
+	public static void main(String[] args) {
+		//Some config and service classes
+	}
+
+}
